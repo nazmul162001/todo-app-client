d3b95c9af5138a76dbb903eadcdc4e55086c5f8e
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-31 09:43:33 +0600

    setUp folder structure & explore todo

---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className="text-center text-red-500 text-5xl font-bold">My Todo App</h1>
+    </div>
+  );
+};
+
+export default Homepage;
